f5633ddb389300d1fb9e2778a2925f586cc1f273
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-24 13:51:52 -0700

    change input to textarea, add labels, add styles

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -50,39 +50,48 @@ function Experience({setPageContent, setInputShow, inputShow}) {
     <form action="" onSubmit={submitHandler}>
         <h2>Experience</h2>
         {inputShow.experience && (
-        <div>
-            <input 
-            type="text"
-            name='workPlace'
-            value={experienceFormData.workPlace}
-            onChange={inputHandler}
-            placeholder='Company'
-            />
-            <input 
-            type="text"
-            name='title'
-            value={experienceFormData.title}
-            onChange={inputHandler}
-            placeholder='Position'
-            />
-            <input 
-            type="date"
-            name='startDate'
-            value={experienceFormData.startDate}
-            onChange={inputHandler}
-            />
-            <input 
-            type="date"
-            name='endDate'
-            value={experienceFormData.endDate}
-            onChange={inputHandler}
-            />
-            <input 
+        <div className="input-container">
+            <div>
+
+                <input 
+                type="text"
+                name='workPlace'
+                value={experienceFormData.workPlace}
+                onChange={inputHandler}
+                placeholder='Company'
+                />
+                <input 
+                type="text"
+                name='title'
+                value={experienceFormData.title}
+                onChange={inputHandler}
+                placeholder='Position'
+                />
+            </div>
+            <div className="date-container">
+                <label htmlFor="startDate">Start Date: </label>
+                <input 
+                type="date"
+                name='startDate'
+                value={experienceFormData.startDate}
+                onChange={inputHandler}
+                />
+                <label htmlFor="endDate">End Date: </label>
+                <input 
+                type="date"
+                name='endDate'
+                value={experienceFormData.endDate}
+                onChange={inputHandler}
+                />
+
+            </div>
+            <textarea 
             type="text"
             name='description'
             value={experienceFormData.description}
             onChange={inputHandler}
             placeholder='Responsibility'
+            className="description-input"
             />
         </div>
         )}
