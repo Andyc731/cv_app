2a7380bd4bf1e9b267e6b4c271741027de1ab156
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-24 19:08:37 -0700

    add new button, change experienceFormData into array

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -2,38 +2,36 @@ import { useState } from "react";
 
 function Experience({setPageContent, setInputShow, inputShow}) {
 
-    const [experienceFormData, setExperienceFormData] = useState({
+    const [experienceFormData, setExperienceFormData] = useState([{
         workPlace: '',
         title: '',
         date: '',
         description: ''
-    })
+    }])
 
     const submitHandler = (event) => {
         event.preventDefault();
     
         setPageContent((prevPage) => ({
             ...prevPage,
-            experience: [{
-                workPlace: experienceFormData.workPlace,
-                title: experienceFormData.title,
-                startDate: experienceFormData.startDate,
-                endDate: experienceFormData.endDate,
-                description: experienceFormData.description
-            }]
+            experience: experienceFormData
         }))
     }
 
-      const inputHandler = (event) => {
+    const inputHandler = (event, index) => {
         const {name, value} = event.target;
 
-        setExperienceFormData((prevForm) => ({
-            ...prevForm,
-            [name]: value
-        }))
+        setExperienceFormData((prevForm) => {
+            const updatedForm = [...prevForm]
+            updatedForm[index] = {
+                ...updatedForm[index],
+                [name]: value
+            }
+            return updatedForm;
+        })
     }
 
-      const buttonHandler = (event) => {
+    const buttonHandler = (event) => {
         event.target.type === 'button' ? 
         setInputShow(prevShow => ({
             ...prevShow,
@@ -46,55 +44,75 @@ function Experience({setPageContent, setInputShow, inputShow}) {
         }))
     }
 
+    const newButtonHandler = () => {
+        setExperienceFormData((prevForm) => {
+            const updatedForm = [...prevForm]
+            updatedForm.push(
+                {
+                    workPlace: '',
+                    title: '',
+                    date: '',
+                    description: ''
+                }
+            )
+            return updatedForm;
+        })
+    }
+
     return (
     <form action="" onSubmit={submitHandler}>
         <h2>Experience</h2>
         {inputShow.experience && (
-        <div className="input-container">
-            <div>
+        <div>
+            {experienceFormData.map((experience, index)=> (
+                <div key={index} className="input-container">
+                    <div>
+                        <input 
+                        type="text"
+                        name='workPlace'
+                        value={experience.workPlace}
+                        onChange={(e => {inputHandler(e, index)})}
+                        placeholder='Company'
+                        />
+                        <input 
+                        type="text"
+                        name='title'
+                        value={experience.title}
+                        onChange={(e => {inputHandler(e, index)})}
+                        placeholder='Position'
+                        />
+                    </div>
+                    <div className="date-container">
+                        <label htmlFor="startDate">Start Date: </label>
+                        <input 
+                        type="date"
+                        name='startDate'
+                        value={experience.startDate}
+                        onChange={(e => {inputHandler(e, index)})}
+                        />
+                        <label htmlFor="endDate">End Date: </label>
+                        <input 
+                        type="date"
+                        name='endDate'
+                        value={experience.endDate}
+                        onChange={(e => {inputHandler(e, index)})}
+                        />
+        
+                    </div>
+                    <textarea 
+                    type="text"
+                    name='description'
+                    value={experience.description}
+                    onChange={(e => {inputHandler(e, index)})}
+                    placeholder='Responsibility'
+                    className="description-input"
+                    />
+                </div>
+            ))}
 
-                <input 
-                type="text"
-                name='workPlace'
-                value={experienceFormData.workPlace}
-                onChange={inputHandler}
-                placeholder='Company'
-                />
-                <input 
-                type="text"
-                name='title'
-                value={experienceFormData.title}
-                onChange={inputHandler}
-                placeholder='Position'
-                />
-            </div>
-            <div className="date-container">
-                <label htmlFor="startDate">Start Date: </label>
-                <input 
-                type="date"
-                name='startDate'
-                value={experienceFormData.startDate}
-                onChange={inputHandler}
-                />
-                <label htmlFor="endDate">End Date: </label>
-                <input 
-                type="date"
-                name='endDate'
-                value={experienceFormData.endDate}
-                onChange={inputHandler}
-                />
-
-            </div>
-            <textarea 
-            type="text"
-            name='description'
-            value={experienceFormData.description}
-            onChange={inputHandler}
-            placeholder='Responsibility'
-            className="description-input"
-            />
         </div>
         )}
+        <button type="button" name='experience' onClick={newButtonHandler}>New</button>
         <button type='button' name='experience' onClick={buttonHandler}>Edit</button>
         <button type='submit' name='experience' onClick={buttonHandler}>Submit</button>
     </form>
